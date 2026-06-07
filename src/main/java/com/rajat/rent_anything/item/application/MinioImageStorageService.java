37b7c96575f0eb5187141af28c78836d6709086f
Rajat <rajatrajoria0811@gmail.com>
2026-06-07 23:05:03 +0530

    change minio config

--- src/main/java/com/rajat/rent_anything/item/application/MinioImageStorageService.java
+++ src/main/java/com/rajat/rent_anything/item/application/MinioImageStorageService.java
@@ -1,8 +1,9 @@
 package com.rajat.rent_anything.item.application;
 
-import com.rajat.rent_anything.item.config.StorageProperties;
 import com.rajat.rent_anything.item.exceptions.ImageStorageException;
-import io.minio.*;
+import io.minio.MinioClient;
+import io.minio.PutObjectArgs;
+import io.minio.RemoveObjectArgs;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
@@ -13,41 +14,58 @@ import java.util.UUID;
 @Slf4j
 public class MinioImageStorageService implements ImageStorageService {
 
-    private final MinioClient minioClient;
-    private final StorageProperties storageProperties;
+    private static final String BUCKET_NAME = "rent-anything";
+    private static final String MINIO_ENDPOINT = "http://localhost:9000";
 
-    public MinioImageStorageService(MinioClient minioClient, StorageProperties storageProperties) {
+    private final MinioClient minioClient;
+
+    public MinioImageStorageService(MinioClient minioClient) {
         this.minioClient = minioClient;
-        this.storageProperties = storageProperties;
     }
 
     @Override
     public String upload(Long itemId, MultipartFile file) {
+
         try {
+
             String originalFilename = file.getOriginalFilename();
+
             String imageKey = "items/" + itemId + "/" + UUID.randomUUID() + "-" + originalFilename;
-            minioClient.putObject(PutObjectArgs.builder().bucket(storageProperties.getBucketName()).object(imageKey).stream(file.getInputStream(), file.getSize(), -1).contentType(file.getContentType()).build());
+
+            minioClient.putObject(PutObjectArgs.builder().bucket(BUCKET_NAME).object(imageKey).stream(file.getInputStream(), file.getSize(), -1).contentType(file.getContentType()).build());
+
             log.info("Uploaded image for itemId {} with key {}", itemId, imageKey);
+
             return imageKey;
+
         } catch (Exception ex) {
+
             log.error("Failed to upload image for itemId {}", itemId, ex);
+
             throw new ImageStorageException("Failed to upload image to storage");
         }
     }
 
     @Override
     public void delete(String imageKey) {
+
         try {
-            minioClient.removeObject(RemoveObjectArgs.builder().bucket(storageProperties.getBucketName()).object(imageKey).build());
+
+            minioClient.removeObject(RemoveObjectArgs.builder().bucket(BUCKET_NAME).object(imageKey).build());
+
             log.info("Deleted image {}", imageKey);
+
         } catch (Exception ex) {
+
             log.error("Failed to delete image {}", imageKey, ex);
+
             throw new ImageStorageException("Failed to delete image from storage");
         }
     }
 
     @Override
     public String getPublicUrl(String imageKey) {
-        return storageProperties.getEndpoint() + "/" + storageProperties.getBucketName() + "/" + imageKey;
+
+        return MINIO_ENDPOINT + "/" + BUCKET_NAME + "/" + imageKey;
     }
 }
